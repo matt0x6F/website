bcac43ff30acc3930b56b0a9c5af711151487ad3
Matt Ouille <matt.ouille@protonmail.com>
2025-05-31 16:50:57 -0700

    Add error handling component: create error.vue for displaying error messages and a 404 page. Update package.json and package-lock.json to include @tsconfig/node22 and add unit and e2e test scripts.

---
+++ ui/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import PageNotFoundView from '~/views/PageNotFoundView.vue'
+defineProps<{ error: { statusCode: number, message: string } }>()
+</script>
+
+<template>
+  <PageNotFoundView v-if="error.statusCode === 404" />
+  <div v-else class="flex flex-col items-center justify-center min-h-[80vh] text-center p-5">
+    <h1 class="text-5xl font-bold mb-4">Error {{ error.statusCode }}</h1>
+    <p class="mb-8">{{ error.message }}</p>
+    <NuxtLink to="/" class="text-blue-500 underline">Go Home</NuxtLink>
+  </div>
+</template> 
